c22406ee064b4860e6ecbb4e948046c31d6f150a
carlosdizx <carlodiaz@umariana.edu.co>
2021-05-06 17:49:51 -0500

    Initial commit

---
+++ src/main/java/com/inventas/InventasApplication.java
@@ -0,0 +1,13 @@
+package com.inventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inventas/InventasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
