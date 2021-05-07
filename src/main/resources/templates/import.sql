d7390439b209710b5d8c35d2c9871d600631ed83
carlosdizx <carlodiaz@umariana.edu.co>
2021-05-06 23:18:47 -0500

    creamos el archivo import.sql

---
+++ src/main/resources/templates/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO empresa VALUES (3163930876,'1555-IU','155874-BN','arenad@center.com','Arena Center',3163930876);
+INSERT INTO empresa VALUES (3163930876,'1555-CU','1554545-BL','tienda@jjcorreo.com','JJ Tienda',3163930876);
+INSERT INTO empresa VALUES (3163930876,'1555-VB','1550045-HJ','pollos@faraon.com','Pollos Faraon',3163930876);
