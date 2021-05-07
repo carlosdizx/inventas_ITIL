08e242e6328f8525e1c185bd1ea3085e7004c16b
carlosdizx <carlodiaz@umariana.edu.co>
2021-05-06 21:20:55 -0500

    Creado el controlador rest para empresa, tiene los metodos listar, paginar y guardar

--- src/main/java/com/inventas/models/services/api/IEmpresaService.java
+++ src/main/java/com/inventas/models/services/api/IEmpresaService.java
@@ -1,8 +1,17 @@
 package com.inventas.models.services.api;
 
 import com.inventas.models.entity.Empresa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IEmpresaService
 {
+    List<Empresa> findAll();
+
+    Page<Empresa> paginate(Pageable pageable);
+
     Empresa save(Empresa empresa);
 }
--- src/main/java/com/inventas/models/services/impl/EmpresaServiceImpl.java
+++ src/main/java/com/inventas/models/services/impl/EmpresaServiceImpl.java
@@ -4,7 +4,13 @@ import com.inventas.models.dao.IEmpresaDao;
 import com.inventas.models.entity.Empresa;
 import com.inventas.models.services.api.IEmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class EmpresaServiceImpl implements IEmpresaService
@@ -13,6 +19,21 @@ public class EmpresaServiceImpl implements IEmpresaService
     private IEmpresaDao dao;
 
     @Override
+    @Transactional(readOnly = true)
+    public List<Empresa> findAll()
+    {
+        return dao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Empresa> paginate(Pageable pageable)
+    {
+        return dao.findAll(pageable);
+    }
+
+    @Override
+    @Transactional
     public Empresa save(Empresa empresa)
     {
         return dao.save(empresa);
